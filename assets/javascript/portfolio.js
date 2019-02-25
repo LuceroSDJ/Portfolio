18254cf236e109abecc1d395391e03ae89ca88cd
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-02-24 19:41:18 -0600

    updated files

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -4,31 +4,27 @@ showSlides();
 
 function showSlides() {
     var i;
-    //var slides = $('.mySlides');
-    var slides = document.getElementsByClassName('mySlides');
+    var slides = $('.mySlides');
     
     for(i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
     }
     slideIndexNum++;
     if(slideIndexNum > slides.length) {
-        slideIndexNum =1
+        slideIndexNum =1;
     }
     
    slides[slideIndexNum-1].style.display = "block";  
-   setTimeout(showSlides, 4000); // Change image every 4 seconds  
+   // Change image every 3 seconds 
+   setTimeout(showSlides, 3000);  
 }
 
 
-
-
-
-
-
-
-
-
-
-
+$('#preventDefault').click(function(e) {
+    //prevent default behaviour: going to link right away
+    e.preventDefault();
+    var position = $('#preventDefault').offset().top;
+    $('html').animate({scrollTop: position +50}, 'slow');
+});
 
 });
