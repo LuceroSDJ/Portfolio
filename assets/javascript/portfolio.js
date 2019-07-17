4722473e029791aa10d36e25d449b545fb00b3fd
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-07-17 14:31:52 -0500

    added images on slides

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){ 
-var slideIndexNum = 0;
-showSlides();
+    var slideIndexNum = 0;
+    showSlides();
 
-function showSlides() {
-    var i;
-    var slides = $('.mySlides');
-    
-    for(i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
+    function showSlides() {
+        var i;
+        var slides = $('.mySlides');
+        
+        for(i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+        slideIndexNum++;
+        if(slideIndexNum > slides.length) {
+            slideIndexNum =1;
+        }
+        
+    slides[slideIndexNum-1].style.display = "block";  
+    // Change image every 5 seconds 
+    setTimeout(showSlides, 3000);  
     }
-    slideIndexNum++;
-    if(slideIndexNum > slides.length) {
-        slideIndexNum =1;
-    }
-    
-   slides[slideIndexNum-1].style.display = "block";  
-   // Change image every 5 seconds 
-   setTimeout(showSlides, 5000);  
-}
 
 
-$('#preventDefault').click(function(e) {
-    //prevent default behaviour: going to link right away
-    e.preventDefault();
-    var position = $('#preventDefault').offset().top;
-    $('html').animate({scrollTop: position +50}, 'slow');
-});
+    $('#preventDefault').click(function(e) {
+        //prevent default behaviour: going to link right away
+        e.preventDefault();
+        var position = $('#preventDefault').offset().top;
+        $('html').animate({scrollTop: position +50}, 'slow');
+    });
 
 });
